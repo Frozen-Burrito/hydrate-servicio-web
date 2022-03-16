1907c309d3f23d0c090a0ef29f5d7feb6043bda0
FrozenBurrito <f3rm3ndouza@outlook.com>
2022-03-15 19:42:42 -0600

    Incluir build/ de React

---
+++ Migrations/20220316013844_CreacionInicial.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ServicioHydrate.Migrations
+{
+    public partial class CreacionInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
