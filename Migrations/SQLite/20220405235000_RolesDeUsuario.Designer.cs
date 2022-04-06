0a2e7ecf0e5d3a5fa146ce99fa7f88e6cc6ee39e
FrozenBurrito <f3rm3ndouza@outlook.com>
2022-04-05 19:33:00 -0500

    Migraciones SQLite cambios para usuario

--- Migrations/SQLite/20220405235000_RolesDeUsuario.Designer.cs
+++ Migrations/SQLite/20220405235000_RolesDeUsuario.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ServicioHydrate.Data;
 
 namespace ServicioHydrate.Migrations.SQLite
 {
     [DbContext(typeof(ContextoDBSqlite))]
-    partial class ContextoDBSqliteModelSnapshot : ModelSnapshot
+    [Migration("20220405235000_RolesDeUsuario")]
+    partial class RolesDeUsuario
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/SQLite/20220405235000_RolesDeUsuario.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ServicioHydrate.Migrations.SQLite
+{
+    public partial class RolesDeUsuario : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Usuarios",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldDefaultValueSql: "newid()");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RolDeUsuario",
+                table: "Usuarios",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UtilParaUsuariosId",
+                table: "RespuestasUtiles",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ReportesDeUsuariosId",
+                table: "RespuestasReportadas",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "AutorId",
+                table: "Respuestas",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UtilParaUsuariosId",
+                table: "ComentariosUtiles",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ReportesDeUsuariosId",
+                table: "ComentariosReportados",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "AutorId",
+                table: "Comentarios",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RolDeUsuario",
+                table: "Usuarios");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Usuarios",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValueSql: "newid()",
+                oldClrType: typeof(Guid),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UtilParaUsuariosId",
+                table: "RespuestasUtiles",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ReportesDeUsuariosId",
+                table: "RespuestasReportadas",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "AutorId",
+                table: "Respuestas",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UtilParaUsuariosId",
+                table: "ComentariosUtiles",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ReportesDeUsuariosId",
+                table: "ComentariosReportados",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "AutorId",
+                table: "Comentarios",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
