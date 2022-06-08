bb1d06760a67d428eb5770e012ae93f4493808fb
FrozenBurrito <f3rm3ndouza@outlook.com>
2022-06-07 23:51:19 -0500

    aplicar migraciones con cambios a modelos de comentarios

---
+++ API/Migrations/MySQL/20220608044751_ExtenderComentarios.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServicioHydrate.Migrations.MySQL
+{
+    public partial class ExtenderComentarios : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "NecesitaModificaciones",
+                table: "Comentarios",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "ComentariosArchivados",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    IdComentario = table.Column<int>(type: "int", nullable: false),
+                    Motivo = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Fecha = table.Column<string>(type: "varchar(33)", maxLength: 33, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ComentariosArchivados", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ComentariosArchivados");
+
+            migrationBuilder.DropColumn(
+                name: "NecesitaModificaciones",
+                table: "Comentarios");
+        }
+    }
+}
