1cb2a73dd2b37fdd39aef62d867aaad715483b57
FrozenBurrito <f3rm3ndouza@outlook.com>
2022-06-30 11:41:54 -0500

    aplicar migracion de llaves de api

---
+++ API/Migrations/MySQL/20220630163938_AgregarLlavesDeAPI.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServicioHydrate.Migrations.MySQL
+{
+    public partial class AgregarLlavesDeAPI : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LlavesDeAPI",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    IdUsuario = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Llave = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    PeticionesEnMes = table.Column<int>(type: "int", nullable: false),
+                    FechaDeCreacion = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    RolDeAcceso = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LlavesDeAPI", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LlavesDeAPI_Usuarios_IdUsuario",
+                        column: x => x.IdUsuario,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LlavesDeAPI_IdUsuario",
+                table: "LlavesDeAPI",
+                column: "IdUsuario");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LlavesDeAPI");
+        }
+    }
+}
